c6bb6de67cb332d1b7ace48612ada563b4927efc
rekt <197486146+rektoww@users.noreply.github.com>
2026-09-05 13:30:53 +0300

    Complete 'my_family' task

--- python_lab_01/04_my_family.py
+++ python_lab_01/04_my_family.py
@@ -18,3 +18,21 @@ my_family_height = [
 #   Общий рост моей семьи - ХХ см
 
 # TODO здесь ваш код
+
+my_family.append('Папа')
+my_family.append('Мама')
+my_family.append('Старший брат')
+my_family.append('Старшая сестра')
+my_family.append('Я')
+
+my_family_height.append([my_family[0], 184])
+my_family_height.append([my_family[1], 170])
+my_family_height.append([my_family[2], 185])
+my_family_height.append([my_family[3], 172])
+my_family_height.append([my_family[4], 180])
+
+summary_family_height = sum(member[1] for member in my_family_height)
+
+print(f"Рост отца - {my_family_height[0][1]} см")
+print(f"Общий рост моей семьи - {summary_family_height} см")
+
