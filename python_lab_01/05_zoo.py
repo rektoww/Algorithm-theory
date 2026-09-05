0775536c1c94bff2a60a70eb829451ef6b1e29ae
rekt <197486146+rektoww@users.noreply.github.com>
2026-09-05 13:36:40 +0300

    Complete zoo task

--- python_lab_01/05_zoo.py
+++ python_lab_01/05_zoo.py
@@ -8,18 +8,27 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 # и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.insert(1, 'bear')
+print(zoo)
 
 # Добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark']
 # и выведите список на консоль
 # TODO здесь ваш код
 
+zoo += birds
+print(zoo)
 
 # Уберите слона (elephant) из зоопарка
 # и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.remove('elephant')
+print(zoo)
 
 # Выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть 1-индексированными (первая клетка - номер 1).
 # TODO здесь ваш код
+
+print(f"Лев (lion) живёт в клетке {zoo.index('lion') + 1}")
+print(f"Жаворонок (lark) живёт в клетке {zoo.index('lark') + 1}")
